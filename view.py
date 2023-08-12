2e96043bfc58035341f043ab9a7670afb47d3eac
Nina Konopleva <orangemise@gmail.com>
2023-08-12 08:32:41 +0300

    update file model, text, view

---
+++ view.py
@@ -0,0 +1,16 @@
+import text
+from datetime import datetime, date, time
+
+
+def main_menu() -> int:
+    print(text.main_menu)
+    while True:
+        choice = input(text.input_choice)
+        if choice.isdigit() and 0 < int(choice) < 9:
+            return int(choice)
+
+
+def print_message(message: str):
+    print('\n' + '=' * len(message))
+    print(message)
+    print('=' * len(message) + '\n')
